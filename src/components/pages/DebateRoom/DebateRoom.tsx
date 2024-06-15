dcb875fc790875256800bc8938b0be16b24c2aad
jaeyoon3 <wodbs3191@naver.com>
2024-06-16 00:46:39 +0900

    last please

--- src/components/pages/DebateRoom/DebateRoom.tsx
+++ src/components/pages/DebateRoom/DebateRoom.tsx
@@ -403,28 +403,30 @@ const DebateRoom: React.FC<DebateRoomProps> = ({onLeave}) => {
     }, [ruleData]);
 
     useEffect(() => {
-        const orderElement = document.querySelector('.order');
-        if (orderElement && ruleDataRef.current[ruleIndex]) {
-            const currentRule = ruleDataRef.current[ruleIndex];
-            if (currentRule.debater === "end") {
-                orderElement.textContent = ""; // 메시지 없애기
-                clearInterval(intervalRef.current); // 반복 중지
-                return;
+        const interval = setInterval(() => {
+            if (ruleDataRef.current.length > 0) {
+                // Check if debater is "end"
+                if (ruleDataRef.current[ruleIndex]?.debater === "end") {
+                    setRuleIndex(0); // Reset rule index to start from the beginning
+                    return; // Stop further execution for "end" debater
+                }
+
+                setRuleIndex(prevIndex => (prevIndex + 1) % ruleDataRef.current.length);
             }
-            orderElement.textContent = currentRule.msg; // 그 외의 경우 메시지 표시
-        }
+        }, ruleDataRef.current[ruleIndex]?.time * 1000 || 3000); // time이 undefined일 경우 기본 3초
+
+        return () => clearInterval(interval);
     }, [ruleIndex, ruleData]);
 
     useEffect(() => {
-        // ruleData가 업데이트될 때마다 interval 초기화
-        clearInterval(intervalRef.current);
-        if (ruleDataRef.current.length > 0) {
-            // 초기 interval 설정
-            intervalRef.current = setInterval(() => {
-                setRuleIndex(prevIndex => (prevIndex + 1) % ruleDataRef.current.length);
-            }, ruleDataRef.current[ruleIndex]?.time * 1000 || 3000); // time이 undefined일 경우 기본 3초
+        const orderElement = document.querySelector('.order');
+        if (orderElement && ruleDataRef.current[ruleIndex]) {
+            // Set msg to "끝" if debater is "end"
+            const masg = ruleDataRef.current[ruleIndex]?.debater === "end" ? "끝" : ruleDataRef.current[ruleIndex].msg;
+            orderElement.textContent = masg;
         }
-    }, [ruleData]);
+    }, [ruleIndex, ruleData]);
+
 
 
 
